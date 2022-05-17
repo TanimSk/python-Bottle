37fe11eea3bb2d4841d9f0f467cde9715c12e72e
Tanim Sk <sktanim5800@gmail.com>
2022-05-17 13:52:57 +0600

    main file

---
+++ server.py
@@ -0,0 +1,27 @@
+from bottle import Bottle, run, jinja2_template, TEMPLATE_PATH, request, HTTPResponse
+import os
+import json
+
+# TEMPLATE_PATH  -- specify path of template. Bottle will look into view folder for *.tpl files
+TEMPLATE_PATH.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "view")))
+
+app = Bottle()
+
+@app.get('/')
+def form():
+    return jinja2_template('main')
+
+
+@app.post('/post')
+def save_form():
+
+    # get the json data from client
+    data = request.json
+
+    print(data['name'])
+    print(data['email'])
+    
+    return HTTPResponse(status=200)
+
+
+run(app, host='localhost', port=8080, reloader=True)
